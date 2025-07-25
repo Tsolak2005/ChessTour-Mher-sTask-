9b2cca31504ab573634761b06ed2683ddd0674df
Ani Ghazaryan <ani.ghazaryan.0525@gmail.com>
2025-07-25 14:24:58 +0400

    ui 3rd page

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,18 +8,6 @@
 
 void MainWindow::connectFunction()
 {
-<<<<<<< HEAD
-    //stackedwidget
-    ui->stackedWidget->setCurrentIndex(0);
-    QObject::connect(ui->newTournamnetClickButton, &QPushButton::clicked, this, [this]() { ui->stackedWidget->setCurrentIndex(1);});
-    QObject::connect(ui->okPushButton, &QPushButton::clicked, this,  [this]() { ui->stackedWidget->setCurrentIndex(2); });
-
-    //stackedwidget_1
-    //QObject::connect(ui->drawingPushButton, &QPushButton::clicked, this,  [this]() { ui->stackedWidget_2->setCurrentIndex(0); });
-    //ui->stackedWidget_2->setCurrentIndex(0);
-
-
-=======
     //stackedWidget
     ui->stackedWidget->setCurrentIndex(0);
     QObject::connect(ui->newTournamnetClickButton, &QPushButton::clicked, this, [this]() { ui->stackedWidget->setCurrentIndex(1);});
@@ -30,10 +18,7 @@ void MainWindow::connectFunction()
     // Create a container widget inside the scroll area
     QWidget* container = new QWidget();
     container->setLayout(ui->verticalLayoutOfNames);
-    // ui->verticalLayoutOfNames->addWidget(container);
     ui->scrollArea->setWidget(container);
-    // QVBoxLayout* containerLayout = new QVBoxLayout(container);
->>>>>>> d373684247abdb99733615dfc1bf1984213c7d1a
  }
 
 MainWindow::MainWindow(QWidget *parent)
@@ -54,11 +39,8 @@ void MainWindow::on_pushButtonAddName_clicked()
     static int countOfNames = 0;
     QHBoxLayout * horizontalLayoutOfName = new QHBoxLayout();
     QLineEdit * newLine = new QLineEdit();
-<<<<<<< HEAD
-    // MainWindow::ui->stackedWidget->insertWidget(1,newLine);
-    // ui->stackedWidget->setCurrentWidget(newLine);
-    ui->verticalLayoutOfNames->insertWidget(++countOfAddedNames, newLine);
-=======
+
+    ui->verticalLayoutOfNames->insertWidget(++countOfNames, newLine);
     newLine->setMinimumHeight(10);
     QLabel * newLabel = new QLabel();
     newLabel->setMinimumWidth(15);
@@ -66,5 +48,4 @@ void MainWindow::on_pushButtonAddName_clicked()
     horizontalLayoutOfName->addWidget(newLabel);
     horizontalLayoutOfName->addWidget(newLine);
     ui->verticalLayoutOfNames->addLayout(horizontalLayoutOfName);
->>>>>>> d373684247abdb99733615dfc1bf1984213c7d1a
 }
